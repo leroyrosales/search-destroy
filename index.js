4eb3dc6b19390fa7af82444946f8ed2ccf6cb868
leroyrosales <leroyrosales@gmail.com>
2019-06-15 23:12:05 -0500

    Sets up start script, keeps parenthesis consistent.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const $ = cheerio.load('<h2 class="title">Hello world</h2>')
 $('h1.title').text('Hello world!')
 $('h1').addClass('welcome')
 
-fs.writeFile("index.html", $.html(), function(err) {
+fs.writeFile('index.html', $.html(), function(err) {
     if(err) {
         return console.log(err);
     }
 
-    console.log("The file was saved!");
+    console.log('The file was saved!');
 }); 
